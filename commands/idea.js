7aead7f2b936573c6063aab90f981d2ca4c8768a
Tweeno <iTweeno@gmail.com>
2019-10-13 22:36:51 +0100

    i did more stuff yey

--- commands/idea.js
+++ commands/idea.js
@@ -7,7 +7,7 @@ const ideas = fs.readFileSync(path.join(__dirname, "ideadata.json"));
 const storedideas = JSON.parse(ideas);
 
 const {
-    prefix
+  prefix
 } = require('../config.json');
 
 const Discord = require('discord.js')
@@ -15,120 +15,133 @@ const Discord = require('discord.js')
 const client = require("../client.js");
 
 storedideas.sort(function (a, b) {
-    return new Date(a.date) - new Date(b.date)
+  return new Date(a.date) - new Date(b.date)
 })
 
 module.exports = {
-    name: 'idea',
-    description: 'ur gay',
-    execute(message) {
-        //random idea and fetching nam
-        if (message.content === "!idea random") {
-            const idk = Math.floor(Math.random() * storedideas.length)
+  name: 'idea',
+  description: 'ur gay',
+  execute(message) {
+    //random idea and fetching nam
+    if (message.content === "!idea random") {
+      const idk = Math.floor(Math.random() * storedideas.length)
 
-            const today2 = storedideas[idk].date
+      const today2 = storedideas[idk].date
 
-            const today3 = new Date(today2).toLocaleString()
+      const today3 = new Date(today2).toLocaleString()
 
-            const rIdea = storedideas[idk].text
+      const rIdea = storedideas[idk].text
 
-            const id2 = storedideas[idk].thegayassthatwrotethis
+      const id2 = storedideas[idk].thegayassthatwrotethis
 
-            async function idk2() {
-                await client.fetchUser(id2).then(response => {
+      async function idk2() {
+        await client.fetchUser(id2).then(response => {
 
-                    const username = response.username
+          const username = response.username
 
-                    const discordEmbed = new Discord.RichEmbed()
-                        .setColor('#F8534F')
-                        .setTitle(rIdea)
-                        .setFooter('Sent by: ' + username + " at " + today3);
-                    return message.channel.send(discordEmbed)
+          const discordEmbed = new Discord.RichEmbed()
+            .setColor('#F8534F')
+            .setTitle(rIdea)
+            .setFooter('Sent by: ' + username + " at " + today3);
+          return message.channel.send(discordEmbed)
 
 
-                }).catch(error => {
-                    console.log(error)
-                })
+        }).catch(error => {
+          console.log(error)
+        })
 
-            }
+      }
 
-            idk2()
+      idk2()
 
-        } else {
+    } else {
 
-            //latest idea that also fetches name
+      //latest idea that also fetches name
+      if (message.content === "!idea delete") {
+      
+      let aaa = storedideas.find(THEARRAY => THEARRAY.thegayassthatwrotethis === message.author.id)
+      console.log(storedideas.indexOf(aaa))
 
-            if (message.content === "!idea latest") {
+      } 
 
-                let sortedArray = storedideas.sort(function (a, b) {
-                    return new Date(a.date) - new Date(b.date)
-                })
+      else if (message.content === "!idea latest") {
 
-                let lastArray = sortedArray[sortedArray.length - 1];
+        let sortedArray = storedideas.sort(function (a, b) {
+          return new Date(a.date) - new Date(b.date)
+        })
 
-                const today4 = new Date(lastArray.date).toLocaleString()
+        let lastArray = sortedArray[sortedArray.length - 1];
 
-                const idyeet = lastArray.thegayassthatwrotethis
+        const today4 = new Date(lastArray.date).toLocaleString()
 
-                async function idk3() {
-                    await client.fetchUser(idyeet).then(response => {
+        const idyeet = lastArray.thegayassthatwrotethis
 
-                        const username = response.username
+        async function idk3() {
+          await client.fetchUser(idyeet).then(response => {
 
-                        const discordEmbed = new Discord.RichEmbed()
-                            .setColor('#F8534F')
-                            .setTitle(lastArray.text)
-                            .setFooter('Sent by: ' + username + " at " + today4);
-                        return message.channel.send(discordEmbed)
+            const username = response.username
 
-                            .catch(error => {
-                                console.log(error)
-                            })
-                    })
+            const discordEmbed = new Discord.RichEmbed()
+              .setColor('#F8534F')
+              .setTitle(lastArray.text)
+              .setFooter('Sent by: ' + username + " at " + today4);
+            return message.channel.send(discordEmbed)
 
-                }
+              .catch(error => {
+                console.log(error)
+              })
+          })
 
-                idk3()
-
-            } else {
-                //Inserting new ideas
-
-                let blacklist = ["faggot", "nigger", "nigga", "fag", ]
-
-                for (let word of blacklist) {
-                    if (message.content.includes(word)) {
-                        const discordEmbed = new Discord.RichEmbed()
-                            .setColor('#F8534F')
-                            .setTitle(":x: Let's not use that word, shall we?");
-
-                        return message.channel.send(discordEmbed)
-                    }
-                }
-
-                function ideastring() {
-                    return message.content.slice(prefix.length + "idea ".length);
-                }
-                const storedvalue = ideastring()
-
-                const today = new Date();
-
-                let newIdea = {
-                    text: storedvalue,
-                    thegayassthatwrotethis: message.author.id,
-                    date: today
-                };
-
-                storedideas.push(newIdea);
-
-                const returnstring = JSON.stringify(storedideas);
-
-                fs.writeFileSync(path.join(__dirname, "ideadata.json"), returnstring);
-
-                const discordEmbed = new Discord.RichEmbed()
-                    .setColor('#F8534F')
-                    .setTitle("Idea inserted into the database. ")
-                return message.channel.send(discordEmbed)
-            }
         }
+
+        idk3()
+
+      } else {
+        //Inserting new ideas
+
+        let blacklist = ["faggot", "nigger", "nigga", "fag", ]
+
+        for (let person of storedideas) {
+          if (person.thegayassthatwrotethis === message.author.id) {
+            message.channel.send("You can't send more than 1 idea. Do !idea delete do delete your old idea");
+            return;
+          }}
+            
+
+        for (let word of blacklist) {
+          if (message.content.includes(word)) {
+            const discordEmbed = new Discord.RichEmbed()
+              .setColor('#F8534F')
+              .setTitle(":x: Let's not use that word, shall we?");
+
+            return message.channel.send(discordEmbed)
+          }
+        }
+
+        function ideastring() {
+          return message.content.slice(prefix.length + "idea ".length);
+        }
+        const storedvalue = ideastring()
+
+        const today = new Date();
+
+        let newIdea = {
+          text: storedvalue,
+          thegayassthatwrotethis: message.author.id,
+          date: today
+        };
+
+        storedideas.push(newIdea);
+
+        const returnstring = JSON.stringify(storedideas);
+
+        fs.writeFileSync(path.join(__dirname, "ideadata.json"), returnstring);
+
+        const discordEmbed = new Discord.RichEmbed()
+          .setColor('#F8534F')
+          .setTitle("Idea inserted into the database. ")
+        return message.channel.send(discordEmbed)
+      }
     }
+  }
 }
--- commands/reddit.js
+++ commands/reddit.js
@@ -3,125 +3,128 @@ const Discord = require('discord.js')
 const rp = require("request-promise-native");
 
 module.exports = {
-    name: 'reddit',
-    description: 'ur gay',
-    execute(message) {
-        //Before the reddit thingy actually works
-        function WordCount() {
-            let split2 = message.content
-            return split2.split(" ").length;
-        }
-        const discordEmbed = new Discord.RichEmbed()
-            .setColor('#F8534F')
-            .setDescription(":x: Please only insert one word.")
-
-        if (WordCount() > 2) {
-            message.channel.send(discordEmbed)
-        } else {
-            function messageEndswith() {
-                const split = message.content.split(" ");
-
-                return split[split.length - 1];
-            }
-            const discordEmbed = new Discord.RichEmbed()
-                .setColor('#F8534F')
-                .setDescription(":x: Please specify the subreddit you want to search.")
-
-            if (message.content === "!reddit") {
-                message.channel.send(discordEmbed)
-
-            } else {
-
-                //when all of the thingies are gud
-
-                rp("https://www.reddit.com/r/" + messageEndswith() + ".json?limit=1000")
-                    .then(response => {
-
-                        const subreddit = JSON.parse(response);
-
-                        if (messageEndswith() === "fiftyfifty123") {
-                            message.channel.send("Let's not do that")
-                        } else {
-                            const data = subreddit.data;
-
-                            const childrenConst = data.children
-
-                            const randomnumber = Math.floor(Math.random() * childrenConst.length);
-
-                            if (subreddit.error === 404 || data.children.length === 0) {
-                                return message.channel.send(discordEmbed)
-                            }
-                            const nsfwpost = subreddit.data.children[randomnumber].data.over_18;
-
-                            if (nsfwpost && !message.channel.nsfw) {
-                                const discordEmbed = new Discord.RichEmbed()
-                                    .setColor('#F8534F')
-                                    .setDescription(":warning: This channel does not have a NSFW tag!");
-                                message.channel.send(discordEmbed)
-                            } else {
-                                if (nsfwpost && message.channel.id === "416015319736385547") {
-                                    const discordEmbed = new Discord.RichEmbed()
-                                        .setColor('#F8534F')
-                                        .setTitle(":warning: This subreddit is disabled in this channel. If you think this shouldn't be happening please contact the developer");
-                                    message.channel.send(discordEmbed)
-                                } else {
-
-                                    //this code is a fucking mess pls dont spank me daddy
-                                    if (subreddit.data.children[randomnumber].data.is_self) {
-
-                                        if (childrenConst[randomnumber].data.selftext.length > 2048) {
-
-                                            const first2048 = child[randomnumber].data.selftext.slice(0, 2048);
-
-                                            const everythingAfterThat = childrenConst[randomnumber].data.selftext.slice(2048);
-
-                                            const discordEmbed = new Discord.RichEmbed()
-                                                .setColor('#F8534F')
-                                                .setTitle(childrenConst[randomnumber].data.title)
-                                                .setDescription(first2048)
-                                                .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
-
-                                            const discordEmbed2 = new Discord.RichEmbed()
-                                                .setColor('#F8534F')
-                                                .setDescription(everythingAfterThat)
-                                                .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
-
-                                            message.channel.send(discordEmbed)
-                                            message.channel.send(discordEmbed2)
-                                            return;
-
-                                        } else {
-                                            const discordEmbed = new Discord.RichEmbed()
-                                                .setColor('#F8534F')
-                                                .setTitle(childrenConst[randomnumber].data.title)
-                                                .setDescription(childrenConst[randomnumber].data.selftext)
-                                                .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
-                                                .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
-
-                                            return message.channel.send(discordEmbed)
-                                        }
-                                    } else {
-                                        const discordEmbed = new Discord.RichEmbed()
-                                            .setColor('#F8534F')
-                                            .setTitle(childrenConst[randomnumber].data.title)
-                                            .setImage(childrenConst[randomnumber].data.url)
-                                            .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
-                                            .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
-                                        return message.channel.send(discordEmbed)
-                                    }
-                                }
-                            }
-                        }
-                    }).catch(error => {
-                        console.error(error)
-                        const discordEmbed = new Discord.RichEmbed()
-                            .setColor('#F8534F')
-                            .setDescription(":x: Error");
-
-                        message.channel.send(discordEmbed);
-                    });
-
-            }
-        }
+  name: 'reddit',
+  description: 'ur gay',
+  execute(message) {
+    if (message.author.id === "474656347380645889") {
+      return
     }
+
+    //Before the reddit thingy actually works
+    function WordCount() {
+      let split2 = message.content
+      return split2.split(" ").length;
+    }
+    const discordEmbed = new Discord.RichEmbed()
+      .setColor('#F8534F')
+      .setDescription(":x: Please only insert one word.")
+
+    if (WordCount() > 2) {
+      message.channel.send(discordEmbed)
+    } else {
+      function messageEndswith() {
+        const split = message.content.split(" ");
+
+        return split[split.length - 1];
+      }
+      
+      if (message.content === "!reddit") {
+        const discordEmbed = new Discord.RichEmbed()
+        .setColor('#F8534F')
+        .setDescription(":x: Please specify the subreddit you want to search.")
+        message.channel.send(discordEmbed)
+      } else {
+
+        //when all of the thingies are gud
+
+        rp("https://www.reddit.com/r/" + messageEndswith() + ".json?limit=1000")
+          .then(response => {
+
+            const subreddit = JSON.parse(response);
+
+            if (messageEndswith() === "fiftyfifty123") {
+              message.channel.send("Let's not do that")
+            } else {
+              const data = subreddit.data;
+
+              const childrenConst = data.children
+
+              const randomnumber = Math.floor(Math.random() * childrenConst.length);
+
+              if (subreddit.error === 404 || data.children.length === 0) {
+                return message.channel.send(discordEmbed)
+              }
+              const nsfwpost = subreddit.data.children[randomnumber].data.over_18;
+
+              if (nsfwpost && !message.channel.nsfw) {
+                const discordEmbed = new Discord.RichEmbed()
+                  .setColor('#F8534F')
+                  .setDescription(":warning: This channel does not have a NSFW tag!");
+                message.channel.send(discordEmbed)
+              } else {
+                if (nsfwpost && message.channel.id === "416015319736385547") {
+                  const discordEmbed = new Discord.RichEmbed()
+                    .setColor('#F8534F')
+                    .setTitle(":warning: This subreddit is disabled in this channel. If you think this shouldn't be happening please contact the developer");
+                  message.channel.send(discordEmbed)
+                } else {
+
+                  //this code is a fucking mess pls dont spank me daddy
+                  if (subreddit.data.children[randomnumber].data.is_self) {
+
+                    if (childrenConst[randomnumber].data.selftext.length > 2048) {
+
+                      const first2048 = childrenConst[randomnumber].data.selftext.slice(0, 2048);
+
+                      const everythingAfterThat = childrenConst[randomnumber].data.selftext.slice(2048);
+
+                      const discordEmbed = new Discord.RichEmbed()
+                        .setColor('#F8534F')
+                        .setTitle(childrenConst[randomnumber].data.title)
+                        .setDescription(first2048)
+                        .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
+
+                      const discordEmbed2 = new Discord.RichEmbed()
+                        .setColor('#F8534F')
+                        .setDescription(everythingAfterThat)
+                        .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
+
+                      message.channel.send(discordEmbed)
+                      message.channel.send(discordEmbed2)
+                      return;
+
+                    } else {
+                      const discordEmbed = new Discord.RichEmbed()
+                        .setColor('#F8534F')
+                        .setTitle(childrenConst[randomnumber].data.title)
+                        .setDescription(childrenConst[randomnumber].data.selftext)
+                        .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
+                        .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
+
+                      return message.channel.send(discordEmbed)
+                    }
+                  } else {
+                    const discordEmbed = new Discord.RichEmbed()
+                      .setColor('#F8534F')
+                      .setTitle(childrenConst[randomnumber].data.title)
+                      .setImage(childrenConst[randomnumber].data.url)
+                      .setFooter("👍 " + childrenConst[randomnumber].data.ups + " | 💬 " + childrenConst[randomnumber].data.num_comments)
+                      .setURL("https://reddit.com" + childrenConst[randomnumber].data.permalink)
+                    return message.channel.send(discordEmbed)
+                  }
+                }
+              }
+            }
+          }).catch(error => {
+            console.error(error)
+            const discordEmbed = new Discord.RichEmbed()
+              .setColor('#F8534F')
+              .setDescription(":x: Error");
+
+            message.channel.send(discordEmbed);
+          });
+
+      }
+    }
+  }
 }
