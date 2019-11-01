17e33b266202b1de4fe8a06d489712009f823bfe
Tweeno <iTweeno@gmail.com>
2019-11-01 18:15:24 +0000

    added a new prefix.json cuz i dont want it in the same place as my key

--- commands/idea.js
+++ commands/idea.js
@@ -8,7 +8,7 @@ const storedideas = JSON.parse(ideas);
 
 const {
   prefix
-} = "!"
+} = require("../prefix.json")
 
 const Discord = require('discord.js')
 
--- commands/random.js
+++ commands/random.js
@@ -1,4 +1,4 @@
-const {prefix} = "!"
+const {prefix} = require("../prefix.json")
 
 
 module.exports = {
